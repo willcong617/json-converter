5bdd1fdb5802af1d34a5b8328e3a558cc4b496bf
willcong617 <98546964+willcong617@users.noreply.github.com>
2023-10-16 11:23:43 -0700

    Update csvCreator.py

--- csvCreator.py
+++ csvCreator.py
@@ -2,7 +2,7 @@ import csv
 
 
 # CSV file path
-Input_csv_file = "C:\\Users\\PH332EH\\OneDrive - EY\Documents\\Carrier\\Toshiba\\Python\\DataInput_withIDs.csv"
+Input_csv_file = "input_file_path?DataInput_withIDs.csv" #made anonymous
 
 # Read data from CSV file
 inputData = []
@@ -15,7 +15,7 @@ print("CSV data has been stored in the inputData variable.")
 
 # Write to output data
 outputData = [["variants.id","variants.items.id","variants.items.quantity"]]
-childSKU_options = ["45289928360249", "45289928393017","45289928425785","45289928458553","45289932390713","45289932423481","45289932456249"]
+childSKU_options = ["45289928360249", "45289928393017","45289928425785","45289928458553","45289932390713","45289932423481","45289932456249"] #changed from original values
 
 for i in range(1, len(inputData)):
     lineData = inputData[i]
@@ -31,7 +31,7 @@ for i in range(1, len(inputData)):
 ## WRITE TO CSV
 
 # CSV file path
-Output_csv_file = "C:\\Users\\PH332EH\\OneDrive - EY\\Documents\\Carrier\\Toshiba\\Python\\Resultant_file.csv"
+Output_csv_file = "output_file_path/Resultant_file.csv" #made anonymous
 
 # Write data to CSV file
 with open(Output_csv_file, mode="w", newline="") as file:
